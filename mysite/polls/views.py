ede3b5bbc3354e541aa5091bd27ca11a419b2d7a
kowsalyasivsamy <kowsalya@calibraint.com>
2019-11-13 23:27:15 +0530

    added urls and views file for polls app

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,30 @@
 from django.http import HttpResponse
 
+from .models import Question
+
+from django.shortcuts import get_object_or_404
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return HttpResponse(question)
+
+def detail(request, question_id):
+    # using shortcuts
+    question = get_object_or_404(Question, pk=question_id)
+    return HttpResponse(question)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
