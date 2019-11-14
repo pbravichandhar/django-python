92af5a7bbab65b3e0c8d51917e1bb5175557d9fb
kowsalyasivsamy <kowsalya@calibraint.com>
2019-11-14 10:36:18 +0530

    polls app functionality complted

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
-from .models import Question
+from .models import Question, Choice
 
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, get_list_or_404
+
+from django.urls import reverse
 
 
 def index(request):
@@ -23,8 +25,22 @@ def detail(request, question_id):
     return HttpResponse(question)
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    choice = get_list_or_404(Choice, question_id=question_id)
+    print (choice, 'kowsi')
+    return HttpResponse(choice)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=2)
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return HttpResponse({
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponse(results(request, question_id))
